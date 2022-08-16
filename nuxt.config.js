e024074f32425d4aaebb3b95f743d9a3a77ea2d0
Bill Horsman <bill@logicalcobwebs.com>
2022-08-16 16:48:06 +0100

    Typo

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,9 +10,9 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Print and Ink are by Bill Horsman' },
+      { hid: 'description', name: 'description', content: 'Print and Ink art by Bill Horsman' },
       { property: 'og:title', content: 'Made by Bill' },
-      { property: 'og:description', content: 'Print and Ink are by Bill Horsman' },
+      { property: 'og:description', content: 'Print and Ink art by Bill Horsman' },
       { property: 'og:url', content: 'https://www.madebybill.co.uk/' },
       { property: 'og:type', content: 'website' }
     ],
